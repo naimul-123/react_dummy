9a92d1ed4ff98edba33df52c82568c6446f21535
Naimul-Islam <naimuliu@gmail.com>
2024-03-29 23:57:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1 className="text-5xl font-bold text-center text-red-500">React dummy project</h1>
+    </div>
+  )
+}
+
+export default App
